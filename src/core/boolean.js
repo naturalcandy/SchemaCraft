478af31f09be1e93b139cb8814210bcb0a5d8b60
Nolawi Gerawork <nolawigerawork@gmail.com>
2023-09-06 16:52:07 -0400

    Sets up dist

--- src/core/boolean.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.booleanType = void 0;
-var booleanType = function () {
-    return {
-        type: 'boolean'
-    };
-};
-exports.booleanType = booleanType;
--- src/core/number.ts
+++ src/core/number.ts
@@ -28,5 +28,3 @@ export const numberType: NumberTypeFunction = (options: NumberTypeOptions = {}) 
         ...options
     };
 };
-  
-
